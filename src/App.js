9fb1331de97a07ac38cedca3029c8aa56333c1b9
Nitish <rananitish37@gmail.com>
2024-07-07 21:16:26 +0530

    Created store for users and implemented userSlice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
+import appStore from "./Utils/appStore";
 
 function App() {
   
   return (
-      <Body />
+      <Provider store={appStore}><Body /></Provider>
   );
 }
 
