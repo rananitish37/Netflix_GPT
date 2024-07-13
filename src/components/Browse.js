b8342734ed0d00cc6a6316036af2dd99f56f0f38
Nitish <rananitish37@gmail.com>
2024-07-14 01:37:24 +0530

    started with building browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,22 +1,9 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../Utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../Utils/movieSlice";
+
 
 const Browse = () =>{
-    const dispatch = useDispatch();
-    const getNowPlayingMovies = async () =>{
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-        const json = await data.json();
-
-        console.log(json);
-        dispatch(addNowPlayingMovies(json.results));
-    };
-
-    useEffect(() =>{
-        getNowPlayingMovies();
-    })
+    useNowPlayingMovies();
     return(
         <div>
             <Header />
